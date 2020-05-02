32ba487ef43bf9ba94e589b288870c1613cbf886
Yuta Sakai <yuta.sakai0313@gmail.com>
2020-05-02 18:44:31 +0900

    Create LocationGetter

---
+++ GViewer/GViewer.Android/LocationGetter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+
+[assembly: Dependency(typeof(GViewer.Droid.LocationGetter))]
+
+namespace GViewer.Droid
+{
+    class LocationGetter : ILocationGettable
+    {
+       public Location GetLocation()
+        {
+
+                var task = Geolocation.GetLastKnownLocationAsync();
+                var location = task.Result; 
+
+                return location;
+
+        }
+    }
+}
